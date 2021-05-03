51638e9624e6f7e670996a0a4e8203c336d07859
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-05-02 22:35:20 -0400

    Create factorial.php

---
+++ factorial.php
@@ -0,0 +1,20 @@
+<?php
+function factorial($input){ 
+  
+// If input is less than or equal to 1, then return 1.
+  if($input <=1) {
+    return 1;
+  }
+
+  // If greater than 1, then make a recursive call and search for the factorial. 
+  return $input * factorial($input-1); 
+}
+
+if(!empty($_POST['number'])){
+
+$input = $_POST['number'];
+
+// Echo the output.
+echo '<br>'. 'The number '.$input.' has a factorial of ' . factorial($input);
+}
+?>

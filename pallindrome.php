35e29d7d0b2a02219b68c008285a5a9d15a06390
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-05-23 23:37:21 -0400

    Update pallindrome.php

--- pallindrome.php
+++ pallindrome.php
@@ -1,6 +1,6 @@
 <?php
 
-function isPalindrome($entry){ 
+function Palindrome_String($entry){ 
     if (strrev($entry) == $entry){ 
         return 1; 
     }
@@ -17,4 +17,18 @@ if(isPalindrome($shark)){
   else { 
 echo "No, not a palindrome."; 
   }
-?> 
+
+
+
+
+// Palindrome for integers.
+function Palindrome_Integers($input) {
+    $reverse = strrev($input);
+        if($reverse == $input) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+}       

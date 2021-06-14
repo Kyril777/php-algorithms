fbdfbc5ecd4fc0bd817db8884ac67c9a6c9e3138
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-06-13 23:51:28 -0400

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,16 +1,16 @@
 function fizzBuzz($n) {
-  $result = array();
-	  	for ($i=1; $i <= $n; $i++) {
-	  		if($i % 5== 0 && $i % 3 ==0 ){
-	  			$result[] = "FizzBuzz";
-	  		}else if($i % 3  == 0){
-	  			$result[] = "Fizz";
-	  		}else if($i % 5 == 0){
-	  			$result[] = "Buzz";
-	  		}else{
-	  			$data[] = $i . '';
-	  		}
-	  	}
-	  	return $result;
-    }
+	$result = array();
+		for ($i=1; $i <= $n; $i++) {
+			if($i % 5== 0 && $i % 3 ==0 ) {
+				$result[] = "FizzBuzz";
+			} else if ($i % 3  == 0) {
+				$result[] = "Fizz";
+			} else if($i % 5 == 0) {
+				$result[] = "Buzz";
+			} else {
+				$result[] = $i . '';
+			}
+		}
+		return $result;
+	}
 }

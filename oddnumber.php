204c771722c77b16209b7b05b9d019383adab56f
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-06-28 23:17:22 -0400

    Create oddnumber.php

---
+++ oddnumber.php
@@ -0,0 +1,13 @@
+
+<?php
+// Checks if $num is odd or even
+function check($num){
+    if($num % 2 == 0){
+        echo "An even num"; 
+    }
+    else{
+        echo "An odd num";
+    }
+}
+  
+?>

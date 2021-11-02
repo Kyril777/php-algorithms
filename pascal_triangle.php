bcbc7fb8ba6164bab92c07f7f1f326b0be126ccc
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-11-01 22:50:49 -0400

    Create pascal_triangle.php

---
+++ pascal_triangle.php
@@ -0,0 +1,18 @@
+<?php
+// $level = ;
+$pascal = array(
+    array(1),
+);
+
+for ($i = 1; $i <= $level; ++$i) {
+    $prevCount = count($pascal[$i-1]);
+    for ($j = 0; $j <= $prevCount; ++$j) {
+        $pascal[$i][$j] = (
+            (isset($pascal[$i-1][$j-1]) ? $pascal[$i-1][$j-1] : 0) + 
+            (isset($pascal[$i-1][$j]) ? $pascal[$i-1][$j] : 0)
+        );
+    }
+}
+var_dump($pascal);
+
+?>

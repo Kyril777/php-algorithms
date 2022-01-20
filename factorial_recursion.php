71d1cb905096dad10ec35a976c63ea31d982743c
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-01-19 23:54:22 -0500

    Update factorial_recursion.php

--- factorial_recursion.php
+++ factorial_recursion.php
@@ -1,10 +1,26 @@
 <?php
-function factorial_recursive($num) {
+// First example
+function factorial_recursive_one($num) {
 
     if ($num < 2) {
         return 1;
     } else {
-        return ($num * factorial($num-1));
+        return ($num * factorial_recurisive_one($num-1));
+    }
+}
+?>
+
+<?
+// Second example
+function factorial_recursive_two($num) {
+   
+    if ($num === 0) {
+        return 1;
+    } else {
+        return $num * factorial_recursive_two($num-1);
     }
 }
 
+echo factorial_recursive_two(5);
+
+?>

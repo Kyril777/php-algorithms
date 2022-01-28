eba5b16f7866b019439a30f2b23bcaf09fdf51b2
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-01-27 23:05:47 -0500

    Update reverseIntegers.php

--- reverseIntegers.php
+++ reverseIntegers.php
@@ -1,13 +1,38 @@
 <?php 
-// Reverse a group of numbers
+// Reverse a group of numbers by using strrev().
   
-function reverseInt($int) {  
+function reverse_int1($int) { 
+  
+    // Convert integer into string.
     $int = (string) $integers;  
+  
+    // Reverse order of integer.
     $revString = strrev($int);    
-    $reverseInt = (int) $revString;   
-    return $reverseInt;  
+  
+    // Convert the string into integer.
+    $reverse_int1 = (int) $revString;   
+  
+    return $reverse_int1;  
 }  
 
-echo reverseInt(47102); 
+echo reverse_int1(47102); 
 
 ?>  
+
+
+<?php
+
+// Reverse a group of numbers with a while loop.
+function reverse_int2($int)
+{
+    $reveral = 0;
+    while ($int > 0)
+      {
+        $reversal = $reversal * 10;
+        $reversal = $reversal + $int % 10;
+        $int = (int)($n/10);
+      }
+     return $reversal;
+}   
+
+echo reverse_int2(38956);

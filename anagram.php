e5b0461986dd0b6dc68f03f0572a339b9bcc7d0d
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-01-31 23:56:34 -0500

    Update anagram.php

--- anagram.php
+++ anagram.php
@@ -1,4 +1,5 @@
- function anagram($string1, $string2) {
+<?php
+function anagram($string1, $string2) {
       if (count_chars($string1, 1) == count_chars($string2, 1)) {
            return "This '" . $string1 . "', '" . $string2 . "' are anagrams.";
       }
@@ -8,3 +9,20 @@
 
   }
     echo anagram('dormitory', 'dirty room');
+
+?>
+
+<?php
+
+<?php   
+    function anagram_2($string1, $string2)
+    {
+        if (count_chars($string1, 1) == count_chars($string_2, 1))
+            return 'An anagram.';
+        else 
+            return 'Not an anagram.';       
+    }
+  
+    // Driver code
+    print_r(is_anagram('dessert', 'stressed')."\n");
+?>

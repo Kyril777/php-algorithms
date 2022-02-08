f3c370aba29488f6d92b2cfed52b0e56e3bc5411
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-02-07 23:47:02 -0500

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,16 +1,19 @@
+/* Task: print integers 1 to N, but print “Fizz” if an integer is divisible by 3, “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is divisible by both 3 and 5*/ 
+
+<?php
 function fizzBuzz($n) {
-	$result = array();
-		for ($i=1; $i <= $n; $i++) {
-			if($i % 5== 0 && $i % 3 ==0 ) {
-				$result[] = "FizzBuzz";
-			} else if ($i % 3  == 0) {
-				$result[] = "Fizz";
-			} else if($i % 5 == 0) {
-				$result[] = "Buzz";
-			} else {
-				$result[] = $i . '';
-			}
-		}
-		return $result;
-	}
+ $result = array();
+  for ($i=1; $i <= $n; $i++) {
+   if($i % 5== 0 && $i % 3 ==0 ) {
+    $result[] = "FizzBuzz";
+   } else if ($i % 3  == 0) {
+    $result[] = "Fizz";
+   } else if($i % 5 == 0) {
+    $result[] = "Buzz";
+   } else {
+    $result[] = $i . '';
+   }
+  }
+  return $result;
+ }
 }

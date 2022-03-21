2ad07db07a2be9527d5447cac1d5aa6b25a25fbb
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-03-20 23:55:56 -0400

    Update factorial.php

--- factorial.php
+++ factorial.php
@@ -18,3 +18,15 @@ $input = $_POST['number'];
 echo '<br>'. 'The number '.$input.' has a factorial of ' . factorial($input);
 }
 ?>
+
+
+
+<?php
+function factorial($number){
+    $factorial = 1;
+    for ($i = 1; $i <= $number; $i++){
+      $factorial = $factorial * $i;
+    }
+    return $factorial;
+}
+?>

e57f0f52f1d2043b933840283496256784b0281d
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-03-28 23:45:23 -0400

    Create insertionsort.php

---
+++ sortingAlgorithms/insertionsort.php
@@ -0,0 +1,15 @@
+<?php
+
+ function insertion_sort($array)
+{
+	for($i=0;$i<count($array);$i++){
+		$val = $array[$i];
+		$j = $i-1;
+		while($j>=0 && $array[$j] > $val){
+			$array[$j+1] = $array[$j];
+			$j--;
+		}
+		$array[$j+1] = $val;
+	}
+return $array;
+}

8d9a975a990224b55633438e5b25454bd9e0779f
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-03-30 23:58:59 -0400

    Update bubblesort.php

--- sortingAlgorithms/bubblesort.php
+++ sortingAlgorithms/bubblesort.php
@@ -15,3 +15,25 @@ function bubbleSort($array)
   }
  }
 }
+
+
+
+function bubblesort( $array ) {
+  $length = count( $array );
+  $comparisons  = 0;
+
+  for ( $i = 0; $i < $length; $i++ ) {
+    for ( $j = 0; $j < $length - 1; $j++ ) {
+      $comparisons++;
+
+      if ( $arr[ $j ] > $array[ $j + 1 ] ) {
+        $tmp            = $array[ $j + 1 ];
+        $array[ $j + 1 ]  = $array[ $j ];
+        $array[ $j ]      = $tmp;
+      } 
+    }
+  } 
+ 
+  echo '<h4>' . $comparisons . ' Comparisons</h4>';
+  return $arr;
+} 

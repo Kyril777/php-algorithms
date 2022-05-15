c23b0db2bae0dcf3c54c1e813ad4e9628a5b38ab
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-14 22:31:17 -0400

    Update pallindrome.php

--- pallindrome.php
+++ pallindrome.php
@@ -18,9 +18,36 @@ if(isPalindrome($shark)){
 echo "No, not a palindrome."; 
   }
 
+?>
 
 
+<?php
+// Palindrome if there are spaces in the string.
+function plaindrome2($string) {
+    // Remove spaces
+    $string = str_replace(' ', '', $string);
 
+    // Remove special characters
+    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+
+    // Lowecase all characters
+    $string = strtolower($string);
+
+    // Reverse the string
+    $reverse = strrev($string);
+
+    if ($string == $reverse) {
+        echo "<p>Palindrome</p>";
+    } 
+    else {
+        echo "</p>Not Palindrome</p>";
+    }
+}
+
+?>
+
+
+<?php
 // Palindrome for integers.
 function Palindrome_Integers($input) {
     $reverse = strrev($input);
@@ -32,3 +59,4 @@ function Palindrome_Integers($input) {
             return false;
         }
 }       
+?>

65842ffabccbb2207f2072435424e1fdb495ae72
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-18 21:21:51 -0400

    Update vowels.php

--- vowels.php
+++ vowels.php
@@ -1,7 +1,17 @@
 <?php 
-
 // Counting the vowels. 
 
 function vowels($string) {
     return substr_count($string, 'a')+substr_count($string, 'e')+substr_count($string, 'i')+substr_count($string, 'o')+substr_count($string, 'u');
 }
+
+
+
+function count_vowels($string)
+{
+    preg_match_all('/[aeiou]/i', $string, $matches);
+    return count($matches[0]);
+}
+print_r(count_vowels('Te amo'));
+
+?>

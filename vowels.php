dce942c7e06a1a27e6186df742381f587928d7f0
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-18 21:22:56 -0400

    Update vowels.php

--- vowels.php
+++ vowels.php
@@ -1,12 +1,12 @@
 <?php 
-// Counting the vowels. 
+// Counting the vowels inside of a string using sybstr_count function. 
 
 function vowels($string) {
     return substr_count($string, 'a')+substr_count($string, 'e')+substr_count($string, 'i')+substr_count($string, 'o')+substr_count($string, 'u');
 }
 
 
-
+// Another function but using preg_match_all function.
 function count_vowels($string)
 {
     preg_match_all('/[aeiou]/i', $string, $matches);

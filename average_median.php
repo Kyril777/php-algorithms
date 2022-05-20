869291a8278cb177ccd88270b87f5dd6adf340c5
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-19 23:51:32 -0400

    Update average_median.php

--- average_median.php
+++ average_median.php
@@ -1,6 +1,6 @@
 <?php
 
-function median($digits=array())
+function median1($digits=array())
 {
 	if (!is_array($digits))
 		$digits = func_get_args();
@@ -10,5 +10,12 @@ function median($digits=array())
 	echo ($middle % 2 != 0) ? $digits{$middle-1} : (($digits{$middle-1}) + $digits{$mid}) / 2;
 }
 
+
+function median2($a) { 
+    sort($a);
+    $c = count($a);
+    $m = floor(($c-1)/2);
+    return ($c % 2) ? $a[$m] : (($a[$m]+$a[$m+1])/2);
+}
 
 ?>

e547af222b81fe8a8c64a61a9b3f774db0a84bb0
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-21 23:58:39 -0400

    Update average.php

--- average.php
+++ average.php
@@ -1,13 +1,11 @@
-/* Mean. */
-
 <?php
+/* Mean. */
 function average_mean($entry)
 {
    if (!is_array($entry)) return false;
 
    return array_sum($entry)/count($entry);
 
-?>
 
 /* Median */
 
@@ -15,3 +13,12 @@ function average_mean($entry)
 
 
 /* Mode */
+function average_mode($arr) {
+  $count = array();
+  foreach ((array)$arr as $val) {
+    if (!isset($count[$val])) { $count[$val] = 0; }
+    $count[$val]++;
+  }
+  arsort($count);
+  return key($count);
+}

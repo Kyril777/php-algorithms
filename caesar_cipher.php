d08b3fd737cb7b5ea4df03223aad883a9348c1b3
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-26 23:57:10 -0400

    Create caesar_cipher.php

---
+++ caesar_cipher.php
@@ -0,0 +1,28 @@
+<?php
+function encrypt($message, $s)
+{
+    $result = "";
+ 
+    // Go through the text.
+    for ($i = 0; $i < strlen($message); $i++)
+    {
+        // Transform each character.
+        if (ctype_upper($message[$i]))
+            $result = $result.chr((ord($message[$i]) +
+                               $s - 65) % 26 + 65);
+ 
+    // Encrypt lowecase letters letters
+    else
+        $result = $result.chr((ord($message[$i]) +
+                           $s - 97) % 26 + 97);
+    }
+ 
+    // Return the resulting string
+    return $result;
+}
+ 
+// Driver Code
+$message = "SECRETLOVERS";
+$s = 4;
+echo encrypt($message, $s);
+?>

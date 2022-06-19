c2b977a4ec4e5f99e3d158c68c64c4f8aea202d3
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-06-18 22:27:46 -0400

    Update quicksort.php

--- sortingAlgorithms/quicksort.php
+++ sortingAlgorithms/quicksort.php
@@ -19,3 +19,28 @@ function quickSort($array){
  }
  return array_merge(quickSort($x),array($n),quickSort($y));
 }
+
+
+function quick_sort_2($my_array)
+ {
+	$loe = $gt = array();
+	if(count($my_array) < 2)
+	{
+		return $my_array;
+	}
+	$pivot_key = key($my_array);
+	$pivot = array_shift($my_array);
+	foreach($my_array as $val)
+	{
+		if($val <= $pivot)
+		{
+			$loe[] = $val;
+		}elseif ($val > $pivot)
+		{
+			$gt[] = $val;
+		}
+	}
+	return array_merge(quick_sort($loe),array($pivot_key=>$pivot),quick_sort($gt));
+}
+
+?>

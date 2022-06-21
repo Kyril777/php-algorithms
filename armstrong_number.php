16c9a32ec9aa9bcfc36eec7c8132734554821860
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-06-21 14:48:34 -0400

    Update armstrong_number.php

--- armstrong_number.php
+++ armstrong_number.php
@@ -20,3 +20,24 @@ else
     echo "No it is not an armstrong number";  
   }  
 ?>  
+
+
+<?php
+function arm_strong_num($num, $order) {
+  $y = $num;
+  $sum = 0;
+
+  while ($y > 0){
+    $x = $y % 10;
+    $sum = $sum + Pow($x, $order);
+    $y = (int)($y/10);
+  }
+
+  if ($num == $sum){
+    echo $num." is a Armstrong Number.\n";
+  } else {
+    echo $num." is not a Armstrong Number.\n";
+  }
+}
+
+?>

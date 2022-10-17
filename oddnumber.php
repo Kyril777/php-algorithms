abe95e274a3664e006957e96795c9fd88df76247
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-10-16 23:32:31 -0400

    Update oddnumber.php

--- oddnumber.php
+++ oddnumber.php
@@ -1,4 +1,3 @@
-
 <?php
 // Checks if $num is odd or even
 function check($num){
@@ -10,4 +9,21 @@ function check($num){
     }
 }
   
+// Recursively check whether the number is even or ddd 
+function check($number){
+    if($number == 0)
+        return 1;
+    else if($number == 1)
+        return 0;
+    else if($number<0)
+        return check(-$number);
+    else
+        return check($number-2);        
+}
+
+$number = 17;
+if(check($number))
+    echo "An even number";
+else
+    echo "An odd number";
 ?>

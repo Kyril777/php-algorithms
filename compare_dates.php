ddee77df9b9c6195ce9e62a7990cd4cf89594e3c
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-11-16 23:37:48 -0500

    Create compare_dates.php

---
+++ compare_dates.php
@@ -0,0 +1,11 @@
+<?php
+$date1 = "1987-09-06";
+$date2 = "1999-01-24";
+  
+function compare_date($date1, $date2){
+if ($date1 > $date2)
+    echo "$date1 is latest than $date2";
+else
+    echo "$date1 is older than $date2";
+}  
+?>

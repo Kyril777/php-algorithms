f33b78bc4c910e5c8e1254184f1cad697aae8081
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-03-05 21:28:14 -0500

    Update splits_names.php

--- splits_names.php
+++ splits_names.php
@@ -1,5 +1,6 @@
-// Function that splits a name into two arrays: a $first_name and a $last_name.
+/* Function that splits a name into an array of $first_name and $last_name.*/
 
+<?php
 function split_name($fullName) {
     $name = trim($fullName);
     $last_name = (strpos($fullName, ' ') === false) ? '' : preg_replace('#.*\s([\w-]*)$#', '$1', $fullName);

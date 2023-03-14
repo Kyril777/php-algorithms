ec99e5e2c5dace5e0a6fbbd0549d400aa5d338f2
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-03-13 23:57:57 -0400

    Create returns_array.php

---
+++ returns_array.php
@@ -0,0 +1,10 @@
+<?php
+    function dofoo() {
+        $array["a"] = "Car";
+        $array["b"] = "Bar";
+        $array["c"] = "Bat";
+        return $array;
+    }
+    
+    $foo = dofoo();
+?>

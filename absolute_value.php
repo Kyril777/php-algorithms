d4d8fd08aad53502aadd01771a9fef36ea7128b0
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-05 23:55:41 -0400

    Update absolute_value.php

--- absolute_value.php
+++ absolute_value.php
@@ -1,4 +1,7 @@
 <?php
-function abs_value($value) {
-  echo abs($value);
+
+function abs_value($int) {
+  echo "The absolute value of $int is ", abs($int);
 }
+
+abs_value(-50); // The absolute value of -50 is 50

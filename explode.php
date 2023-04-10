11173cc2b409fcb1eac398def811798e59a15e6b
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-10 00:00:02 -0400

    Update explode.php

--- explode.php
+++ explode.php
@@ -1,11 +1,8 @@
-// Example 1
-$pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
-$pieces = explode(" ", $pizza);
-echo $pieces[0]; // piece1
-echo $pieces[1]; // piece2
+<?php
 
-// Example 2
-$data = "foo:*:1023:1000::/home/foo:/bin/sh";
-list($user, $pass, $uid, $gid, $gecos, $home, $shell) = explode(":", $data);
-echo $user; // foo
-echo $pass; // *
+function explode($pieces) {
+  echo "The pieces are " explode($pieces);
+}
+
+$pieces  = "piece1 piece2 piece3 piece4 piece5 piece6";
+

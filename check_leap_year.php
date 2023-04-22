57a138cca5feacafac7439e89070c7ac70c06cab
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-04-21 23:49:55 -0400

    Update check_leap_year.php

--- check_leap_year.php
+++ check_leap_year.php
@@ -11,5 +11,13 @@ function checkleapyear($year) {
   }
 }
 
+/* Cleaner version. */
+function checkleapyear($year) { 
+  if((0 == $year % 4) & (0 != $year % 100) | (0 == $year % 400)) {
+    echo "$year is a Leap Year.";    
+  } else {  
+    echo "$year is not a Leap Year.";
+  }
+}
+
 checkleapyear(2021);
-?>

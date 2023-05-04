4ea638ff33e94fe700519507c11e1f54f8ddd793
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-03 22:14:47 -0400

    Update vowels.php

--- vowels.php
+++ vowels.php
@@ -1,17 +1,17 @@
 <?php 
-// Counting the vowels inside of a string using sujbstr_count function. 
-
+// Counting the vowels inside of a string using substr_count function. 
 function vowels($string) {
     return substr_count($string, 'a')+substr_count($string, 'e')+substr_count($string, 'i')+substr_count($string, 'o')+substr_count($string, 'u');
 }
 
+print_r(vowels('The quick brown fox jumped over the head of the lazy dog.')); // 16
 
-// Another function but using preg_match_all function.
+
 function count_vowels($string)
 {
     preg_match_all('/[aeiou]/i', $string, $matches);
     return count($matches[0]);
 }
-print_r(count_vowels('Te amo'));
+print_r(count_vowels('Te amo')); // 3
 
 ?>

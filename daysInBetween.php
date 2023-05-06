0fbe3d7efffc50343c4b9a1c7820a199fbe8327f
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-05 23:33:43 -0400

    Update daysInBetween.php

--- daysInBetween.php
+++ daysInBetween.php
@@ -1,5 +1,4 @@
 <?php
-
 /* Function that calculates number of days between two dates. */
 function daysInBetween($date1, $date2) {
     return date_diff(
@@ -8,5 +7,19 @@ function daysInBetween($date1, $date2) {
     )->format('%a');
 }
 
+print_r(daysInBetween("2022-06-20", "2021-06-22")); // 363
 
-print_r(daysInBetween("2021-06-20", "2022-06-22"));
+?>
+
+<?php
+/* Another approach using strtotime(). */
+function daysInBetween($date1, $date2){ 
+    $difference = strtotime($date2) - strtotime($date1); 
+    return abs(round($difference / 86400)); 
+} 
+
+// Sample dates.
+$date1 = "08-08-2021";        
+$date2 = "02-02-2021";    
+
+print_r(daysInBetween($date1, $date2)); // 187

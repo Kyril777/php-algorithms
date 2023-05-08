6f1a90696d5c59d414124fdcbf7a066537050e84
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-07 22:09:30 -0400

    Update primeChecker.php

--- primeChecker.php
+++ primeChecker.php
@@ -16,3 +16,22 @@ if ($x == 0) {
   echo "A Prime Number.";
 }
 ?>
+
+<?php
+function check_prime($num){
+   if ($num == 1)
+   return 0;
+   for ($i = 2; $i <= $num/2; $i++)
+   {
+      if ($num % $i == 0)
+      return 0;
+   }
+   return 1;
+}
+$num = 19;
+$value = check_prime($num);
+if ($value == 1)
+   echo "It is a prime number";
+else
+   echo "It is a non-prime number"
+?>

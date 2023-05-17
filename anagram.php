196e23f2e31d5dc19183e3005a9c7823e7e29b28
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-16 23:16:14 -0400

    Update anagram.php
    Updated to function to lowercase characters and remove whitespaces in the entered parameters.

--- anagram.php
+++ anagram.php
@@ -1,27 +1,19 @@
 <?php
+/* PHP function to check for anagrams. */
 function anagram($string1, $string2) {
-      if (count_chars($string1, 1) == count_chars($string2, 1)) {
-           return "This '" . $string1 . "', '" . $string2 . "' are anagrams.";
+
+      // Remove all whitespaces including tabs, newlines, etc... Lowecase all characters. Store in new variable.
+      $temp1 = strtolower(preg_replace("/\s+/", "", $string1));
+      $temp2 = strtolower(preg_replace("/\s+/", "", $string2));
+      
+      // Counts the instances of characters for each string. If equal, then the string is an anagram.
+      if (count_chars($temp1, 1) == count_chars($temp2, 1)) {
+            return "The strings '" . $string1 . "' and '" . $string2 . "' are anagrams.";
+      } else {
+            return "These two strings are not anagrams.";
       }
-      else {
-          return "This two strings are not anagrams.";
-      }
+}
 
-  }
-    echo anagram('dormitory', 'dirty room');
-
-?>
-
-
-<?php   
-    function anagram_2($string1, $string2)
-    {
-        if (count_chars($string1, 1) == count_chars($string_2, 1))
-            return 'An anagram.';
-        else 
-            return 'Not an anagram.';       
-    }
-  
-    // Driver code
-    print_r(is_anagram('dessert', 'stressed')."\n");
-?>
+print_r(anagram('dormitory', 'dirty room')); // The strings 'dormitory' and 'dirty room' are anagrams.
+print_r(anagram('dessert', 'stressed')); // These two strings are not anagrams.
+print_r(anagram('Alec Guinness', 'genuine class')); // The strings 'Alec Guinness' and 'genuine class' are anagrams.

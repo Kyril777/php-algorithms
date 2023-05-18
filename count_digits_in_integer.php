fba8c10fb597ee8fb49556d27b3231d916328be7
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-17 22:45:35 -0400

    Update count_digits_in_integer.php
    Added a couple of revisions and corrections.

--- count_digits_in_integer.php
+++ count_digits_in_integer.php
@@ -1,10 +1,12 @@
 <?php
-
-// Function that counts number of digits in integers
+/* Function that counts number of digits in integers. */
 function countDigits($num){
+  
+  // Treat the parameter as an integer.
   $num = (int)$num;
   $count = 0;
 
+  // Use a while loop to count the number of digits.
   while($num != 0){
     $num = (int)($num / 10);
     $count++;
@@ -12,27 +14,35 @@ function countDigits($num){
   return $count;
 }
 
-$x = ;
+$x = 1000;
+echo "$x has ".countDigits($x)." digits"; // 1000 has 4 digits
 
-echo "$x gas: ".countDigits($x)." digits";
 
-
- // A simpler code.
+ /* A simpler code. */
 function countDigit($n)
 {
     $count = 0;
+  
+    // Use while loop to count number of integers. Use the int() to acquire the numeric integer equivalent of $n.
     while ($n != 0)
     {
-        $n = round($n / 10);
-        ++$count;
+        $n = (int)($n / 10);
+        $count++;
     }
     return $count;
 }
 
-// Uses the log function.
+$x = 75551;
+echo "$x has ".countDigit($x)." digits"; // 75551 has 5 digits
+
+
+/* Uses the log function. */
 function countDigitsLog($num){
-  return (int)log10(abs($MyNum)) + 1;
+  return (int)log10(abs($num)) + 1;
 }
+
+$x = 10000;
+echo "$x has ".countDigitsLog($x)." digits"; // 10000 has 5 digits
 
 
 ?>

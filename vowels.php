689657ec7bcfeab6371937f2033f918ef6c72169
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-20 22:40:23 -0400

    Update vowels.php
    Added a third function.

--- vowels.php
+++ vowels.php
@@ -6,12 +6,26 @@ function vowels($string) {
 
 print_r(vowels('The quick brown fox jumped over the head of the lazy dog.')); // 16
 
-
+// A second method.
 function count_vowels($string)
 {
     preg_match_all('/[aeiou]/i', $string, $matches);
     return count($matches[0]);
 }
 print_r(count_vowels('Te amo')); // 3
+
+// A third method.
+function count_vowels($string){
+	$count = 0;
+	for($i=0;$i<strlen($string);$i=$i+1){
+		if($string[$i]=='a'||$string[$i]=='e'||$string[$i]=='i'||$string[$i]=='o'||$string[$i]=='u'){ 
+			$count=$count+1;
+		}
+	}
+	echo "The number of vowels in the string \"$string\" are: ".$count;
+}
+
+count_vowels('aeiou'); // The number of vowels in the string "aeiou" are: 5
+?>
 
 ?>

6dce1ce03fc69c42d1498398c601bbd4d469b772
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-05-24 22:02:26 -0400

    Create sequence_maker.php

---
+++ sequence_maker.php
@@ -0,0 +1,26 @@
+<?php
+/* Function that uses array dereferencing to return key values. */
+function data()
+{
+    $retn_key["a"] = 123;
+    $retn_key["b"] = 456;
+    $retn_key["c"] = 789;
+    $retn_key["d"] = 987;
+    $retn_key["e"] = 654;
+    $retn_key["f"] = 321;
+
+    return $retn_key;
+}
+
+$a = data()["a"];
+$b = data()["b"];
+$c = data()["c"];
+$d = data()["d"];
+$e = data()["e"];
+$f = data()["f"];
+
+echo $d . $c . $c . $e .PHP_EOL;
+echo $b . $f . $a . $a .PHP_EOL;
+echo $f . $d . $f . $e .PHP_EOL;
+
+?>

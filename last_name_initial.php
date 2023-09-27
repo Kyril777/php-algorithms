5c78ed3f3ed918e010caf205e978f832977538f3
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-09-27 00:20:06 -0400

    Update last_name_initial.php

--- last_name_initial.php
+++ last_name_initial.php
@@ -9,3 +9,22 @@ echo last_name_initial("Adnar"); // Output: A
 echo last_name_initial("Simpson"); // Output: S
 echo last_name_initial("Neal"); // Output: N
 echo last_name_initial("Knight"); // Output: K
+
+
+// Function that divides names into two companies per their last name initial.
+function last_name_initial($lastname) {
+    $first_letter = substr($lastname, 0, 1);
+    $pattern1 = '/^[A-Ma-m]+$/';
+    $pattern2 = '/^[K-Zk-z]+$/';
+    if(preg_match($pattern1, $first_letter) == true) {
+    	return "Person belongs in the first batch.";
+    } elseif (preg_match($pattern2, $first_letter) == true) {
+    	return "Person belongs in the second batch.";
+    	
+    }
+}
+
+echo last_name_initial("Adnar"); // Output: Person belongs in the first batch.
+echo last_name_initial("Simpson"); // Output: Person belongs in the second batch.
+echo last_name_initial("Neal"); // Output: Person belongs in the second batch.
+echo last_name_initial("Knight"); // Output: Person belongs in the first batch.
